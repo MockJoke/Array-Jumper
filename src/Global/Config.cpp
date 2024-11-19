0b9ca317b7b4bd0a302c723bd39c316e7863e49c
kharva <parvadesai404@gmail.com>
2024-11-20 00:50:49 +0530

    [Fix] resources paths in config

--- src/Global/Config.cpp
+++ src/Global/Config.cpp
@@ -3,30 +3,31 @@
 namespace Global
 {
     // Textures
-    const sf::String Config::array_jumper_bg_texture_path = "../resources/textures/array_jumper_bg.png";
-    const sf::String Config::box_texture_path = "../resources/textures/box.png";
-    const sf::String Config::target_texture_path = "../resources/textures/target.png";
-    const sf::String Config::character_texture_path = "../resources/textures/character.png";
-    const sf::String Config::instructions_button_texture_path = "../resources/textures/instructions_button.png";
-    const sf::String Config::letter_one_texture_path = "../resources/textures/letter_one.png";
-    const sf::String Config::letter_two_texture_path = "../resources/textures/letter_two.png";
-    const sf::String Config::letter_three_texture_path = "../resources/textures/letter_three.png";
-    const sf::String Config::menu_button_texture_path = "../resources/textures/menu_button.png";
-    const sf::String Config::obstacle_01_texture_path = "../resources/textures/obstacle_01.png";
-    const sf::String Config::obstacle_02_texture_path = "../resources/textures/obstacle_02.png";
-    const sf::String Config::outscal_logo_texture_path = "../resources/textures/outscal_logo.png";
-    const sf::String Config::play_button_texture_path = "../resources/textures/play_button.png";
-    const sf::String Config::quit_button_texture_path = "../resources/textures/quit_button.png";
+    const sf::String Config::array_jumper_bg_texture_path = "../../resources/textures/array_jumper_bg.png";
+    const sf::String Config::box_texture_path = "../../resources/textures/box.png";
+    const sf::String Config::target_texture_path = "../../resources/textures/target.png";
+    const sf::String Config::character_texture_path = "../../resources/textures/character.png";
+    const sf::String Config::instructions_button_texture_path = "../../resources/textures/instructions_button.png";
+    const sf::String Config::letter_one_texture_path = "../../resources/textures/letter_one.png";
+    const sf::String Config::letter_two_texture_path = "../../resources/textures/letter_two.png";
+    const sf::String Config::letter_three_texture_path = "../../resources/textures/letter_three.png";
+    const sf::String Config::menu_button_texture_path = "../../resources/textures/menu_button.png";
+    const sf::String Config::obstacle_01_texture_path = "../../resources/textures/obstacle_01.png";
+    const sf::String Config::obstacle_02_texture_path = "../../resources/textures/obstacle_02.png";
+    const sf::String Config::outscal_logo_texture_path = "../../resources/textures/outscal_logo.png";
+    const sf::String Config::play_button_texture_path = "../../resources/textures/play_button.png";
+    const sf::String Config::quit_button_texture_path = "../../resources/textures/quit_button.png";
 
     // Fonts
-    const sf::String Config::bubble_bobble_font_path = "../resources/fonts/bubbleBobble.ttf";
-    const sf::String Config::zorque_font_path = "../resources/fonts/zorque.ttf";
+    const sf::String Config::bubble_bobble_font_path = "../../resources/fonts/bubbleBobble.ttf";
+    const sf::String Config::zorque_font_path = "../../resources/fonts/zorque.ttf";
 
     // Sounds
-    const sf::String Config::background_music_path = "../resources/sounds/background_music.mp3";
-    const sf::String Config::button_click_sound_path = "../resources/sounds/button_click_sound.wav";
-    const sf::String Config::death_sound_path = "../resources/sounds/death.wav";
-    const sf::String Config::jump_sound_path = "../resources/sounds/jump.wav";
-    const sf::String Config::level_complete_sound_path = "../resources/sounds/level_complete.wav";
-    const sf::String Config::move_sound_path = "../resources/sounds/move.wav";
+    const sf::String Config::background_music_path = "../../resources/sounds/background_music.mp3";
+    const sf::String Config::button_click_sound_path = "../../resources/sounds/button_click_sound.wav";
+    const sf::String Config::death_sound_path = "../../resources/sounds/death.wav";
+    const sf::String Config::jump_sound_path = "../../resources/sounds/jump.wav";
+    const sf::String Config::level_complete_sound_path = "../../resources/sounds/level_complete.wav";
+    const sf::String Config::move_sound_path = "../../resources/sounds/move.wav";
+    const sf::String Config::game_won_sound_path = "../../resources/sounds/game_won_sound.wav";
 }
